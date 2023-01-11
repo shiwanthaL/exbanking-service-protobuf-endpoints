0a65d09e06c369e5dc5e0d4acc42c66762f58ce2
Your Name <shivantha.msc@gmail.com>
2023-01-11 23:12:46 +0530

    sprint initalizer pacakge structure

---
+++ src/main/java/com/yolo/practical/exbankingservice/ExbankingServiceProtobufEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.yolo.practical.exbankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExbankingServiceProtobufEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExbankingServiceProtobufEndpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yolo/practical/exbankingservice/ExbankingServiceProtobufEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolo.practical.exbankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExbankingServiceProtobufEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
