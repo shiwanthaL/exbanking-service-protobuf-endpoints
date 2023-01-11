3f52ec21981b2c155053b75c6e3a1c9d86baa274
Your Name <shivantha.msc@gmail.com>
2023-01-12 00:47:33 +0530

    service : create_user, get_users service layer end-point completed

---
+++ src/main/java/com/yolo/practical/exbankingservice/service/BankService.java
@@ -0,0 +1,43 @@
+package com.yolo.practical.exbankingservice.service;
+
+import com.github.javafaker.Faker;
+import com.google.protobuf.Empty;
+import com.yolo.practical.bankingservice.proto.BankingServiceGrpc;
+import com.yolo.practical.bankingservice.proto.CreateUserRequest;
+import com.yolo.practical.bankingservice.proto.CreateUserResponse;
+import com.yolo.practical.bankingservice.proto.GetUsersResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@GrpcService
+public class BankService extends BankingServiceGrpc.BankingServiceImplBase {
+    private Faker data;
+    private final List<CreateUserResponse> accountHolders = new ArrayList<>();
+
+    public void create_user(CreateUserRequest request, StreamObserver<CreateUserResponse> response){
+        CreateUserResponse createUser = CreateUserResponse.newBuilder().
+
+                setFullName(request.getFullName()).
+                setEmail(request.getEmail()).
+                setPassport(request.getPassport()).
+                setAccountNo(data.finance().creditCard()).
+                setIbanNo(data.finance().iban()).
+                setSwiftCode(data.address().cityPrefix()+data.number().digits(3)).
+                setBankName(data.company().name()).
+                setBranchName(data.address().cityName()).
+                setBalance(data.number().numberBetween(10000,50000)).build();
+
+        accountHolders.add(createUser);
+        response.onNext(createUser);
+        response.onCompleted();
+    }
+
+    public void get_users(Empty request, StreamObserver<GetUsersResponse> response){
+        response.onNext(GetUsersResponse.newBuilder().addAllUsers(accountHolders).build());
+        response.onCompleted();
+    }
+
+}
